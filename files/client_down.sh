6c9495516b2008b7c1b6c47be7dfcfd8f55450e4
aa65535 <aa65535@live.com>
2014-10-21 10:18:48 +0800

    update client script
    use the syslog standard format to log
    use more reliable way of removing the iptables rules

--- files/client_down.sh
+++ files/client_down.sh
@@ -6,37 +6,48 @@
 # all key value pairs in ShadowVPN config file will be passed to this script
 # as environment variables, except password
 
+PID=$(cat $pidfile)
+loger() {
+	echo "$(date '+%c') down.$1 ShadowVPN[$PID] $2"
+}
+
 # uncomment if you want to turn off IP forwarding
 # sysctl -w net.ipv4.ip_forward=0>/dev/null 2>&1
 
-# turn off NAT over VPN and old gateway
-/etc/init.d/firewall restart>/dev/null 2>&1
-
 # get old gateway and old interface
-echo "$(date) [DOWN] reading old gateway and old interface from saved file"
-old_gw=$(cat /tmp/old_gw) && old_intf=$(cat /tmp/old_intf) || {
-  echo "$(date) [DOWN] failed to get interface from saved file, check up.sh"
+loger info "reading old gateway and old interface from saved file"
+old_gw=$(cat /tmp/old_gw) && old_intf=$(cat /tmp/old_intf)
+
+if [ -z "$old_intf" ]; then
+  loger error "failed to get interface from saved file, check up.sh"
   exit 1
-}
+fi
+
+# turn off NAT over VPN and old gateway
+loger notice "turn off NAT over $intf and $old_intf"
+iptables -t nat -D POSTROUTING -o $intf -j MASQUERADE
+iptables -D FORWARD -i $old_intf -o $intf -j ACCEPT
+iptables -D FORWARD -i $intf -o $old_intf -j ACCEPT
+[ -f /tmp/iptables ] && eval $(cat /tmp/iptables)
 
 # change routing table
 route del $server $old_intf
 route del default
 if [ -z "$old_gw" ]; then
   route add default $old_intf
-  echo "$(date) [DOWN] default route changed to $old_intf"
+  loger notice "default route changed to $old_intf"
 else
   route add default gw $old_gw
-  echo "$(date) [DOWN] default route changed to $old_gw"
+  loger notice "default route changed to $old_gw"
 fi
 
 # remove chnroute rules
 if [ -f /tmp/routes ]; then
   sed -i 's#route add#route del#g' /tmp/routes
   ip -batch /tmp/routes
-  echo "$(date) [DOWN] chnroute rules removed"
+  loger notice "chnroute rules have been removed"
 fi
 
-rm -f /tmp/old_gw /tmp/old_intf /tmp/routes
+rm -f /tmp/old_gw /tmp/old_intf /tmp/routes /tmp/iptables
 
-echo "$(date) [DOWN] done"
+loger info "$0 completed"
--- files/client_up.sh
+++ files/client_up.sh
@@ -6,6 +6,11 @@
 # all key value pairs in ShadowVPN config file will be passed to this script
 # as environment variables, except password
 
+PID=$(cat $pidfile)
+loger() {
+	echo "$(date '+%c') up.$1 ShadowVPN[$PID] $2"
+}
+
 # turn on IP forwarding
 sysctl -w net.ipv4.ip_forward=1>/dev/null 2>&1
 
@@ -14,7 +19,7 @@ ifconfig $intf 10.7.0.2 netmask 255.255.255.0
 ifconfig $intf mtu $mtu
 
 # get current gateway and interface
-echo "$(date) [UP] get gateway and interface from route table"
+loger info "get gateway and interface from route table"
 eval $(ip route show | awk '/^default/ {
   for (i=1; i<=NF; i++) {
     if ($i == "via") { printf("old_gw=%s;", $(i+1)) }
@@ -23,29 +28,31 @@ eval $(ip route show | awk '/^default/ {
 }')
 
 if [ -z "$old_intf" ]; then
-  echo "$(date) [UP] failed to get interface from route table"
+  loger error "can not get interface from route table"
   exit 1
 fi
 
 # if current interface is tun, read from saved file.
 if [ "$old_intf" = "$intf" ]; then
-  echo "$(date) [UP] reading gateway and interface from saved file"
+  loger notice "reading gateway and interface from saved file"
   old_gw=$(cat /tmp/old_gw) && old_intf=$(cat /tmp/old_intf) || {
-    echo "$(date) [UP] can not read gateway or interface, check up.sh"
+    loger error "can not read gateway or interface, check up.sh"
     exit 1
   }
 fi
 
 # save gateway and interface to file
-echo $old_gw > /tmp/old_gw
-echo $old_intf > /tmp/old_intf
-echo "$(date) [UP] save gateway and interface to file"
+echo $old_gw >/tmp/old_gw
+echo $old_intf >/tmp/old_intf
+loger info "save gateway and interface to file"
 
 # turn on NAT over VPN and old gateway
+loger notice "turn on NAT over $intf and $old_intf"
 iptables -t nat -A POSTROUTING -o $intf -j MASQUERADE
 iptables -A FORWARD -i $old_intf -o $intf -j ACCEPT
 iptables -A FORWARD -i $intf -o $old_intf -j ACCEPT
-iptables -D delegate_forward -j reject>/dev/null 2>&1
+iptables -D delegate_forward -j reject>/dev/null 2>&1 && \
+echo "iptables -A delegate_forward -j reject" >/tmp/iptables
 
 # change routing table
 if [ -z "$old_gw" ]; then
@@ -57,7 +64,7 @@ else
 fi
 route del default
 route add default gw 10.7.0.1
-echo "$(date) [UP] default route changed to 10.7.0.1"
+loger notice "default route changed to 10.7.0.1"
 
 # chnroute list file, You can specify a custom routes list file.
 chnroute=/etc/chinadns_chnroute.txt
@@ -65,9 +72,9 @@ chnroute=/etc/chinadns_chnroute.txt
 # load chnroute rules
 if [ -f $chnroute ]; then
   awk -v suf="$suf" '$1 ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}/\
-    {printf("route add %s %s\n",$1,suf)}' $chnroute > /tmp/routes
+    {printf("route add %s %s\n",$1,suf)}' $chnroute >/tmp/routes
   ip -batch /tmp/routes
-  echo "$(date) [UP] chnroute rules loaded"
+  loger notice "chnroute rules have been loaded"
 fi
 
-echo "$(date) [UP] done"
+loger info "$0 completed"
