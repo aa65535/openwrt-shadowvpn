ed1cb44394f3c55377cb417e54bbf690b9fc3cbd
aa65535 <aa65535@live.com>
2014-12-26 20:04:07 +0800

    update client script

--- files/client_down.sh
+++ files/client_down.sh
@@ -4,28 +4,26 @@
 # All key value pairs in ShadowVPN config file will be passed to this script
 # as environment variables, except password.
 
-PID=$(cat $pidfile)
+PID=$(cat $pidfile 2>/dev/null)
 loger() {
 	echo "$(date '+%c') down.$1 ShadowVPN[$PID] $2"
 }
 
-# Get old gateway and old interface
-loger info "reading old gateway and old interface from saved file"
-old_gw=$(cat /tmp/old_gw) && old_intf=$(cat /tmp/old_intf)
+# Get old value from saved file
+old_gw=$(cat /tmp/shadowvpn/old_gw 2>/dev/null)
+old_intf=$(cat /tmp/shadowvpn/old_intf 2>/dev/null)
+route_mode=$(cat /tmp/shadowvpn/route_mode 2>/dev/null)
 
 if [ -z "$old_intf" ]; then
-	loger error "can't get interface from saved file, check up.sh"
+	loger error "Can't get interface from saved file, check up.sh"
 	exit 1
 fi
 
 # Turn off NAT over VPN
-loger notice "turn off NAT over $intf"
+loger notice "Turn off NAT over $intf"
 iptables -t nat -D POSTROUTING -o $intf -j MASQUERADE
 iptables -D FORWARD -o $intf -j ACCEPT
 iptables -D FORWARD -i $intf -j ACCEPT
-
-# Get uci setting
-route_mode=$(uci get shadowvpn.@shadowvpn[-1].route_mode 2>/dev/null)
 
 # Change routing table
 route del $server $old_intf
@@ -33,20 +31,20 @@ if [ "$route_mode" != 2 ]; then
 	route del default
 	if [ -z "$old_gw" ]; then
 		route add default $old_intf
-		loger notice "default route changed to $old_intf"
+		loger notice "Default route changed to $old_intf"
 	else
 		route add default gw $old_gw
-		loger notice "default route changed to $old_gw"
+		loger notice "Default route changed to $old_gw"
 	fi
 fi
 
 # Remove route rules
-if [ -f /tmp/routes ]; then
-	sed -i 's#route add#route del#g' /tmp/routes
-	ip -batch /tmp/routes
-	loger notice "route rules have been removed"
+if [ -f /tmp/shadowvpn/routes ]; then
+	sed -i 's#route add#route del#g' /tmp/shadowvpn/routes
+	ip -batch /tmp/shadowvpn/routes
+	loger notice "Route rules have been removed"
 fi
 
-rm -f /tmp/old_gw /tmp/old_intf /tmp/routes
+rm -rf /tmp/shadowvpn
 
-loger info "$0 completed"
+loger info "Script $0 completed"
--- files/client_up.sh
+++ files/client_up.sh
@@ -4,17 +4,23 @@
 # All key value pairs in ShadowVPN config file will be passed to this script
 # as environment variables, except password.
 
-PID=$(cat $pidfile)
+PID=$(cat $pidfile 2>/dev/null)
 loger() {
 	echo "$(date '+%c') up.$1 ShadowVPN[$PID] $2"
 }
+
+mkdir -p /tmp/shadowvpn
 
 # Configure IP address and MTU of VPN interface
 ifconfig $intf 10.7.0.2 netmask 255.255.255.0
 ifconfig $intf mtu $mtu
 
+# Get uci setting
+route_mode=$(uci get shadowvpn.@shadowvpn[-1].route_mode 2>/dev/null)
+route_file=$(uci get shadowvpn.@shadowvpn[-1].route_file 2>/dev/null)
+
 # Get current gateway and interface
-loger info "get gateway and interface from route table"
+loger info "Get gateway and interface from route table"
 eval $(ip route show | awk '/^default/ {
 	for (i=1; i<=NF; i++) {
 		if ($i == "via") { printf("old_gw=%s;", $(i+1)) }
@@ -23,34 +29,31 @@ eval $(ip route show | awk '/^default/ {
 }')
 
 if [ -z "$old_intf" ]; then
-	loger error "can't get interface from route table"
+	loger error "Can't get interface from route table"
 	exit 1
 fi
 
 # If current interface is tun, read from saved file.
 if [ "$old_intf" = "$intf" ]; then
-	loger notice "reading gateway and interface from saved file"
-	old_gw=$(cat /tmp/old_gw) && old_intf=$(cat /tmp/old_intf)
+	loger notice "Reading gateway and interface from saved file"
+	old_gw=$(cat /tmp/shadowvpn/old_gw 2>/dev/null)
+	old_intf=$(cat /tmp/shadowvpn/old_intf 2>/dev/null)
 	if [ -z "$old_intf" ]; then
-		loger error "can't read gateway or interface, check up.sh"
+		loger error "Can't read gateway or interface, check up.sh"
 		exit 1
 	fi
 fi
 
-# Save gateway and interface to file
-echo $old_gw >/tmp/old_gw
-echo $old_intf >/tmp/old_intf
-loger info "save gateway and interface to file"
+# Save current value to file
+echo $old_gw >/tmp/shadowvpn/old_gw
+echo $old_intf >/tmp/shadowvpn/old_intf
+echo $route_mode >/tmp/shadowvpn/route_mode
 
 # Turn on NAT over VPN
-loger notice "turn on NAT over $intf"
+loger notice "Turn on NAT over $intf"
 iptables -t nat -A POSTROUTING -o $intf -j MASQUERADE
 iptables -I FORWARD 1 -o $intf -j ACCEPT
 iptables -I FORWARD 1 -i $intf -j ACCEPT
-
-# Get uci setting
-route_mode=$(uci get shadowvpn.@shadowvpn[-1].route_mode 2>/dev/null)
-route_file=$(uci get shadowvpn.@shadowvpn[-1].route_file 2>/dev/null)
 
 # Add routing table
 if [ -z "$old_gw" ]; then
@@ -65,7 +68,7 @@ fi
 if [ "$route_mode" != 2 ]; then
 	route del default
 	route add default gw 10.7.0.1
-	loger notice "default route changed to 10.7.0.1"
+	loger notice "Default route changed to 10.7.0.1"
 else
 	suf="via 10.7.0.1 dev $intf"
 fi
@@ -73,9 +76,9 @@ fi
 # Load route rules
 if [ "$route_mode" != 0 -a -f "$route_file" ]; then
 	awk -v suf="$suf" '$1 ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}/\
-		{printf("route add %s %s\n",$1,suf)}' $route_file >/tmp/routes
-	ip -batch /tmp/routes
-	loger notice "route rules have been loaded"
+		{printf("route add %s %s\n",$1,suf)}' $route_file >/tmp/shadowvpn/routes
+	ip -batch /tmp/shadowvpn/routes
+	loger notice "Route rules have been loaded"
 fi
 
-loger info "$0 completed"
+loger info "Script $0 completed"
