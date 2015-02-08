f1aa96f227ceb860fc669da4ea2f7f11171472ed
aa65535 <aa65535@live.com>
2015-02-08 11:52:05 +0800

    fix hotplug

--- files/client_up.sh
+++ files/client_up.sh
@@ -13,17 +13,19 @@ loger() {
 ifconfig $intf 10.7.0.2 netmask 255.255.255.0
 ifconfig $intf mtu $mtu
 
+# Get original gateway
+device=$(ip route show 0/0 | grep via | sed -e 's/.* dev \([^ ]*\).*/\1/')
+gateway=$(ip route show 0/0 | grep via | sed -e 's/.* via \([^ ]*\).*/\1/')
+loger info "The default gateway: via $gateway dev $device"
+
 # Get uci setting
 route_mode=$(uci get shadowvpn.@shadowvpn[-1].route_mode 2>/dev/null)
 route_file=$(uci get shadowvpn.@shadowvpn[-1].route_file 2>/dev/null)
 
 # Save uci setting
+uci set shadowvpn.@shadowvpn[-1].device_save=$device
 uci set shadowvpn.@shadowvpn[-1].route_mode_save=$route_mode
 uci commit shadowvpn
-
-# Get original gateway
-gateway=$(ip route show 0/0 | grep via | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
-loger info "The original default gateway: $gateway"
 
 # Turn on NAT over VPN
 iptables -t nat -A POSTROUTING -o $intf -j MASQUERADE
